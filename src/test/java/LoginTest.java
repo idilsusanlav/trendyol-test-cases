2776f629b42a1fcddc3734570941fe88214c52e2
İdilsu Sanlav <115869980+idilsusanlav@users.noreply.github.com>
2024-02-24 23:07:05 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -16,8 +16,8 @@ public class LoginTest extends BaseTest {
         driver.findElement(By.cssSelector(".account-user > .icon-container")).click();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         Assert.assertTrue(driver.findElement(By.className("lr-title")).isDisplayed());
-        driver.findElement(By.id("login-email")).sendKeys("fridamaurice@gmail.com");
-        driver.findElement(By.id("login-password-input")).sendKeys("1234567");
+        driver.findElement(By.id("login-email")).sendKeys(USERNAME);
+        driver.findElement(By.id("login-password-input")).sendKeys(WRONGPASSWORD);
         //driver.findElement(By.xpath(("//*[@id=\"login-register\"]/div[3]/div[2]/form/button/span[contains(text(),'Giriş Yap')]"))).click();
         driver.findElement(By.cssSelector(".q-primary.q-fluid.q-button-medium.q-button.submit")).click();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
